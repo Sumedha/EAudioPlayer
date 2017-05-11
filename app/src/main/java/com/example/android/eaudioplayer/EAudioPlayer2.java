bfd08cf122b717bdfdd586fb881e18b2a03d6d4d
Sumedha Dharmasiri <Sumedha.Dharmasiri@gmail.com>
2017-05-11 20:29:02 +0200

    Minor changes to songs!

---
+++ app/src/main/java/com/example/android/eaudioplayer/EAudioPlayer2.java
@@ -0,0 +1,126 @@
+package com.example.android.eaudioplayer;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Nightwish on 2017-05-11.
+ */
+
+public class EAudioPlayer2 extends Activity {
+
+    private static final int NOTIFICATION_EX = 1;
+    public TextView songName, duration;
+    private MediaPlayer mediaPlayer;
+    private double timeElapsed = 0, finalTime = 0;
+    private int forwardTime = 2000, backwardTime = 2000;
+    private Handler durationHandler = new Handler();
+    private SeekBar seekbar;
+    //handler to change seekBarTime
+    private Runnable updateSeekBarTime = new Runnable() {
+        public void run() {
+            //get current position
+            timeElapsed = mediaPlayer.getCurrentPosition();
+            //set seekbar progress
+            seekbar.setProgress((int) timeElapsed);
+            //set time remaing
+            double timeRemaining = finalTime - timeElapsed;
+            duration.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining), TimeUnit.MILLISECONDS.toSeconds((long) timeRemaining) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining))));
+            //repeat yourself that again in 100 miliseconds
+            durationHandler.postDelayed(this, 100);
+        }
+    };
+    private NotificationManager notificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //set the layout of the Activity
+        setContentView(R.layout.activity_eaudio_player);
+        //initialize views
+        initializeViews();
+    }
+
+    public void initializeViews() {
+        songName = (TextView) findViewById(R.id.songName);
+        mediaPlayer = MediaPlayer.create(this, R.raw.george_michael_careless_whisper);
+        finalTime = mediaPlayer.getDuration();
+        duration = (TextView) findViewById(R.id.songDuration);
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+        songName.setText("Careless Whisper");
+
+        seekbar.setMax((int) finalTime);
+        seekbar.setClickable(false);
+    }
+
+    // play mp3 song
+    public void play(View view) {
+        mediaPlayer.start();
+        timeElapsed = mediaPlayer.getCurrentPosition();
+        seekbar.setProgress((int) timeElapsed);
+        durationHandler.postDelayed(updateSeekBarTime, 100);
+    }
+
+    public void rewind(View view) {
+        if (mediaPlayer == null) {
+            return;
+        }
+        if ((timeElapsed + backwardTime) <= finalTime) {
+            timeElapsed = timeElapsed - backwardTime;
+            //seek to the exact second of the track
+            mediaPlayer.seekTo((int) timeElapsed);
+        }
+    }
+
+    // pause mp3 song
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+
+    // go forward at forwardTime seconds
+    public void forward(View view) {
+        //check if we can go forward at forwardTime seconds before song endes
+        if ((timeElapsed + forwardTime) <= finalTime) {
+            timeElapsed = timeElapsed + forwardTime;
+            //seek to the exact second of the track
+            mediaPlayer.seekTo((int) timeElapsed);
+        }
+    }
+
+    public void exit(View view) {
+        ImageButton exit = (ImageButton) findViewById(R.id.playerexit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            this.moveTaskToBack(true);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing()) {
+            notificationManager.cancel(NOTIFICATION_EX);
+        }
+    }
+}
